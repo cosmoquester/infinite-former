8b5c980b1e08c5921183f011b44591e4ce28a90c
cosmoquester <cosmoquester@gmail.com>
2023-01-03 13:35:51 +0900

    refactor: Cleaning code

--- finetune_gpt2/src/transformers/models/gpt2/long_term_attention.py
+++ finetune_gpt2/src/transformers/models/gpt2/long_term_attention.py
@@ -10,7 +10,7 @@ import torch.distributions as dist
 from .basis_functions import GaussianBasisFunctions
 from .continuous_sparsemax import ContinuousSparsemax
 from .continuous_softmax import ContinuousSoftmax
-from typing import Literal
+from typing import Literal, List
 
 class GaussianBasisFunctionsModule(nn.Module, GaussianBasisFunctions):
     def __init__(self, mu, sigma) -> None:
@@ -20,9 +20,12 @@ class GaussianBasisFunctionsModule(nn.Module, GaussianBasisFunctions):
         self.register_buffer("sigma", sigma.unsqueeze(0))
 
 class LongTermAttention(nn.Module):
+    #: number of samples used for update
+    nb_samples=512
+
     def __init__(self, head_dim: int, memory_length: int, attn_func: Literal["softmax", "sparsemax"], 
                   attn_num_basis: int, attn_drop: float, use_infinite_memory: bool, n_heads: int, 
-                  use_affines: bool, use_mask: bool, mask_type: Literal["affine", "cnn"],
+                  use_affines: bool, mask_type: Literal["affine", "cnn", "none"],
                   mask_dropout: float, use_kl_regularizer: bool, sigma_0: float, mu_0: float, 
                   use_sticky_memories: bool, **kwargs):
         """
@@ -34,7 +37,6 @@ class LongTermAttention(nn.Module):
         :param use_infinite_memory: use infinity memory if true
         :param n_heads: the number of heads
         :param use_affines: use affine transform
-        :param use_mask: use masking
         :param mask_type: masking type
         :param mask_dropout: masking dropout
         :param use_kl_regularizer: use kl regularizer
@@ -49,19 +51,15 @@ class LongTermAttention(nn.Module):
         self.attn_num_basis = attn_num_basis
         self.attn_func = attn_func
         self.n_heads = n_heads
-        
-        self.use_kl_regularizer = use_kl_regularizer
         self.sigma_0 = sigma_0
         self.mu_0 = mu_0
+        self.mask_type=mask_type
 
         self.use_affines=use_affines # whether mu, sigma should be computed using affine transformations
-
-        self.use_mask=use_mask
-        self.mask_type=mask_type
-        self.mask_dropout_value=mask_dropout
-        self.use_sticky_memories=use_sticky_memories
+        self.use_kl_regularizer = use_kl_regularizer
         self.use_infinite_memory = use_infinite_memory
- 
+        self.use_sticky_memories=use_sticky_memories
+
         self.proj_query = nn.Linear(n_heads * head_dim, n_heads * head_dim, bias=False)
         self.proj_key = nn.Linear(n_heads * head_dim, n_heads * head_dim, bias=False)
         self.proj_value = nn.Linear(n_heads * head_dim, n_heads * head_dim, bias=False)
@@ -69,22 +67,15 @@ class LongTermAttention(nn.Module):
         self.attn_out = nn.Linear(n_heads * head_dim, n_heads * head_dim, bias=False)
         self.attn_dropout = nn.Dropout(attn_drop)
 
-        if self.use_mask:
-            if self.mask_type=='affine':
-                self.mask_net=nn.Linear(memory_length,memory_length)
-            elif self.mask_type=='cnn':
-                self.mask_net=torch.nn.Conv1d(n_heads*head_dim, n_heads*head_dim,3,padding=1)
+        if self.mask_type=='affine':
+            self.mask_net=nn.Linear(memory_length,memory_length)
+        elif self.mask_type=='cnn':
+            self.mask_net=torch.nn.Conv1d(n_heads*head_dim, n_heads*head_dim,3,padding=1)
+        self.mask_dropout=nn.Dropout(mask_dropout)
 
-        if self.mask_dropout_value>0:
-            self.mask_dropout=nn.Dropout(self.mask_dropout_value)
-
-        self.nb_samples=512 # number of samples used for update
-        self.tau = 0.5 #compressing factor
-
-        self.ridge_penalty=0.5 # ridge penalty
         padding = True
 
-        if self.use_affines:
+        if use_affines:
             self.mu = nn.Linear(attn_num_basis, 1, bias=False)
             self.sigma = nn.Linear(attn_num_basis, 1, bias=False)
             self.softplus = torch.nn.Softplus()
@@ -95,7 +86,7 @@ class LongTermAttention(nn.Module):
             attn_num_basis += (len(sigmas) - attn_num_basis % len(sigmas))
 
         # get positions for memory vectors
-        basis_mu, basis_sigma = LongTermAttention.get_gaussian_basis_functions(attn_num_basis, sigmas)
+        basis_mu, basis_sigma = self.get_gaussian_basis_functions(attn_num_basis, sigmas)
         self.psi = GaussianBasisFunctionsModule(mu=basis_mu, sigma=basis_sigma)
         self.register_buffer("basis_mu", basis_mu)
         self.register_buffer("basis_sigma", basis_sigma)
@@ -122,58 +113,64 @@ class LongTermAttention(nn.Module):
         # compute basis functions
         Gs = self.compute_G(memory_length, self.psi, positions, padding=padding) # [L,N]
         self.register_buffer("Gs", Gs)
-        self.positions = positions[int(memory_length/2):-int(memory_length/2)]
+        self.register_buffer("positions", positions[int(memory_length/2):-int(memory_length/2)])
 
         # compute samples for memory update
         if self.use_infinite_memory:
-            tm_tau = torch.arange(1,self.nb_samples+1).float()
-            tm_l = torch.arange(self.nb_samples+1,memory_length+self.nb_samples+1).float()
-            tm_tau = tm_tau*self.tau/self.nb_samples # positions of old vectors
-            tm_l = self.tau + (1-self.tau)*(tm_l-self.nb_samples)/memory_length # positions of new vectors
-            positions_inf = torch.cat([tm_tau, tm_l],0) # positions
-
-            if padding:
-                if memory_length % 2:
-                    shift = 1 / float(memory_length+self.nb_samples)
-                    positions_pad_ = torch.linspace(-.5+shift, 0, 2*(memory_length+self.nb_samples)-1)
-                else:
-                    shift = 1 / float(2*memory_length+self.nb_samples)
-                    positions_pad = torch.linspace(-.5+shift, 1.5-shift, 2*(memory_length+self.nb_samples))
-                positions_pad_ = torch.FloatTensor([i for i in positions_pad if i<0])
-                positions_pad__ = torch.FloatTensor([i for i in positions_pad if i>1])
-                positions_inf = torch.cat([positions_pad_,positions_inf,positions_pad__], dim=0)
-
-            self.samples=None
-            for t in tm_tau:
-                if self.samples is None:
-                    self.samples = self.psi.evaluate(t/self.tau)
-                else:
-                    self.samples = torch.cat([self.samples,self.psi.evaluate(t/self.tau)], dim=0)
-
-            # compute G for the infinite case
-            G_inf = self.compute_G(self.nb_samples+memory_length, self.psi, positions_inf, padding=padding) #[L+nb_samples,N]
+            samples, G_inf = self.get_infinity_components(padding)
+            self.register_buffer("samples", samples)
             self.register_buffer("G_inf", G_inf)
-
+            
             if self.use_sticky_memories:
                 self.register_buffer("bins", torch.linspace(0,1,129)) #self.positions
                 self.nb_bins_cat=1
                 self.bins_cat = dist.Categorical(torch.ones(self.nb_bins_cat))
 
+    def get_infinity_components(self, padding: bool, tau: float=0.5):
+        """
+        :param padding: padding
+        :param tau: compressing factor
+        """
+        tm_tau = torch.arange(1,self.nb_samples+1).float()
+        tm_l = torch.arange(self.nb_samples+1,self.memory_length+self.nb_samples+1).float()
+        tm_tau = tm_tau*tau/self.nb_samples # positions of old vectors
+        tm_l = tau + (1-tau)*(tm_l-self.nb_samples)/self.memory_length # positions of new vectors
+        positions_inf = torch.cat([tm_tau, tm_l],0) # positions
+
+        if padding:
+            if self.memory_length % 2:
+                shift = 1 / float(self.memory_length+self.nb_samples)
+                positions_pad_ = torch.linspace(-.5+shift, 0, 2*(self.memory_length+self.nb_samples)-1)
+            else:
+                shift = 1 / float(2*self.memory_length+self.nb_samples)
+                positions_pad = torch.linspace(-.5+shift, 1.5-shift, 2*(self.memory_length+self.nb_samples))
+            positions_pad_ = torch.FloatTensor([i for i in positions_pad if i<0])
+            positions_pad__ = torch.FloatTensor([i for i in positions_pad if i>1])
+            positions_inf = torch.cat([positions_pad_,positions_inf,positions_pad__], dim=0)
+
+        samples = torch.cat([ self.psi.evaluate(t / tau) for t in tm_tau], dim=0)
+
+        # compute G for the infinite case
+        G_inf = self.compute_G(self.nb_samples+self.memory_length, self.psi, positions_inf, padding=padding) #[L+nb_samples,N]
+
+        return samples, G_inf
+
+
     @staticmethod
-    def get_gaussian_basis_functions(nb_basis: int, sigmas) -> torch.Tensor:
+    def get_gaussian_basis_functions(nb_basis: int, sigmas: List[float]) -> torch.Tensor:
         mu, sigma = torch.meshgrid(torch.linspace(0, 1, nb_basis // len(sigmas)), torch.Tensor(sigmas))
         mu = mu.flatten()
         sigma = sigma.flatten()
         assert mu.size(0) == nb_basis
         return mu, sigma
 
-    def compute_G(self, l: int, psi: GaussianBasisFunctionsModule, positions: torch.Tensor, padding=True) -> torch.Tensor:
+    def compute_G(self, l: int, psi: GaussianBasisFunctionsModule, positions: torch.Tensor, ridge_penalty: float = 0.5, padding=True) -> torch.Tensor:
         F = torch.zeros(self.attn_num_basis, positions.size(0))
 
         F[:, :] = psi.evaluate(positions.unsqueeze(1)).t()
 
         I = torch.eye(self.attn_num_basis)
-        G = F.t().matmul((F.matmul(F.t()) + self.ridge_penalty * I).inverse())
+        G = F.t().matmul((F.matmul(F.t()) + ridge_penalty * I).inverse())
 
         if padding:
             if l % 2:
@@ -250,10 +247,9 @@ class LongTermAttention(nn.Module):
         qlen = query.size(2) #query length
 
         k = k.permute(0,2,1) # [B,e,L]
-        if self.mask_dropout_value>0:
-            k = self.mask_dropout(k)
+        k = self.mask_dropout(k)
 
-        if self.use_mask:
+        if self.mask_type != "none":
             reg_mask=torch.sigmoid(self.mask_net(k))
             k = k*reg_mask
 
